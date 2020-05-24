5c51fbf23fcc0593b666d7d756a159ea76532653
Christine Clemens <christineclemens.tesol@gmail.com>
2020-05-23 22:14:12 -0400

    Added variables and created rudimentary prompts.

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,40 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+var tempPass;
+var passLength;
+var numbers = "0123456789";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var specialChar = "!@#$%^&*()-_=+{}[]|\;:<>?/";
+
+function generatePassword() {
+
+  var askLength = prompt("How many characters would you like your password to be (8-128)?");
+    console.log(askLength);
+      if (askLength >= 8 && askLength <= 128) {
+        passLength = Number(askLength);
+      }
+  var askUppercase = confirm("Would you like to include uppercase characters?");
+    console.log(askUppercase);
+      if (askUppercase == true) {
+        tempPass = tempPass + numbers;
+      }
+  var askNumbers = confirm("Would you like to include numbers?");
+    console.log(askNumbers);
+      if (askNumbers == true) {
+        tempPass = tempPass + numbers;
+      }
+  var askSpecialChar = confirm("Would you like to include special characters?");
+    console.log(askSpecialChar);
+      if (askSpecialChar == true) {
+        tempPass = tempPass + specialChar;
+      }
+
+}
